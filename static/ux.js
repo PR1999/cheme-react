e57ba574660fb325a3d90416fe387f8165615eb9
Puck Roelink <puckroelink@outlook.com>
2021-09-02 15:51:52 +0200

    Removed unused files and code

--- static/ux.js
+++ static/ux.js
@@ -1,31 +1,5 @@
 let colorlist = ['#1abc9c', '#2ecc71', '#3498db', '#9b59b6', '#f1c40f', '#e67e22', '#e74c3c']
-function setadvkinetics() {
-    let advKineticsCheckbox = document.querySelector('input[value="adv"]');
-    let n1slider = document.getElementById("n1sliderbox");
-    let n2slider = document.getElementById("n2sliderbox")
-    if(advKineticsCheckbox.checked) {
-        n1slider.style.visibility = 'visible';
-        n1slider.style.display = 'flex';
-        n2slider.style.visibility = 'visible';
-        n2slider.style.display = 'flex';
-        document.getElementById('func1').value = "k1 *(ca^n1)*(cb^n2)";
-        document.getElementById('func1box').style.visibility = 'visible';
-        document.getElementById('func1box').style.display = 'flex';
-        document.getElementById('func1dropdownbox').style.visibility = 'hidden';
-        document.getElementById('func1dropdownbox').style.display = 'none';
-    }
-     else {
-        n1slider.style.visibility = 'hidden';
-        n1slider.style.display = 'none';
-        n2slider.style.visibility = 'hidden';
-        n2slider.style.display = 'none';
-        document.getElementById('func1').value = "k1 * ca * cb";
-        document.getElementById('func1box').style.visibility = 'hidden';
-        document.getElementById('func1box').style.display = 'none';
-        document.getElementById('func1dropdownbox').style.visibility = 'visible';
-        document.getElementById('func1dropdownbox').style.display = 'flex';
-    }
-}
+
 
 function showhideplot(id) {
     let plot = plotmap.get(componentidmap.get(id))
